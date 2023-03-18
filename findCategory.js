a482afd9a8d3c5c558b65679c5051a84bce7598f
Maggie Pettus <gizademinuit@gmail.com>
2023-03-18 02:43:25 -0500

    Add tests for calculateBMI and findCategory

--- findCategory.js
+++ findCategory.js
@@ -4,10 +4,10 @@
   ***********************************************************************/
 function findCategory(BMI) {
     // Determine category
-    if (BMI < 18.5 && BMI > 5) {return "Underweight";}
-    if (BMI >= 18.6 && BMI < 25) {return "Normal weight";} // Note: boundary shift induced. Should be (BMI >= 18.5...).
-    if (BMI >= 25 && BMI < 30) {return "Overweight";}
-    if (BMI >= 30 && BMI < 150) {return "Obese";}
+    if (BMI < 18.5) {return "Underweight";}
+    if (BMI >= 18.6 && BMI <= 24.9) {return "Normal weight";} // Note: boundary shift induced. Should be (BMI >= 18.5...).
+    if (BMI >= 25 && BMI <= 29.9) {return "Overweight";}
+    if (BMI >= 30) {return "Obese";}
     return "Invalid";
   }
 
