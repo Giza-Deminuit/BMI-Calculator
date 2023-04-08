8372de8e8119d7e5312006bf0c9e86810de6fe50
Maggie Pettus <gizademinuit@gmail.com>
2023-04-08 18:10:59 -0500

    Add prettier

--- findCategory.js
+++ findCategory.js
@@ -1,14 +1,22 @@
-  /**********************************************************************  
+/**********************************************************************  
   This function uses the BMI calculated in calculateBMI to determine
   the correct category of weight and returns it to getData().
   ***********************************************************************/
 function findCategory(BMI) {
-    // Determine category
-    if (BMI < 18.5) {return "Underweight";}
-    if (BMI >= 18.5 && BMI <= 24.9) {return "Normal weight";} // Note: boundary shift induced. Should be (BMI >= 18.5...).
-    if (BMI >= 25 && BMI <= 29.9) {return "Overweight";}
-    if (BMI >= 30) {return "Obese";}
-    return "Invalid";
+  // Determine category
+  if (BMI < 18.5) {
+    return "Underweight";
   }
+  if (BMI >= 18.5 && BMI <= 24.9) {
+    return "Normal weight";
+  } // Note: boundary shift induced. Should be (BMI >= 18.5...).
+  if (BMI >= 25 && BMI <= 29.9) {
+    return "Overweight";
+  }
+  if (BMI >= 30) {
+    return "Obese";
+  }
+  return "Invalid";
+}
 
-  module.exports = findCategory;
+module.exports = findCategory;
--- findCategory.test.js
+++ findCategory.test.js
@@ -1,48 +1,48 @@
-const fCat = require('./findCategory.js');
+const fCat = require("./findCategory.js");
 
 // Using 1x1
 
 test('Testing lower boundary of "Normal weight". ON point.', () => {
-    expect(fCat(18.5)).toBe("Normal weight");
+  expect(fCat(18.5)).toBe("Normal weight");
 });
 
 test('Testing lower boundary of "Normal weight". OFF point.', () => {
-    expect(fCat(18.4)).toBe("Underweight");
+  expect(fCat(18.4)).toBe("Underweight");
 });
 
 test('Testing "Normal weight". Interior point.', () => {
-    expect(fCat(21.7)).toBe("Normal weight");
+  expect(fCat(21.7)).toBe("Normal weight");
 });
 
 test('Testing upper boundary of "Normal weight". ON point.', () => {
-    expect(fCat(24.9)).toBe("Normal weight");
+  expect(fCat(24.9)).toBe("Normal weight");
 });
 
 test('Testing upper boundary of "Normal weight". OFF point.', () => {
-    expect(fCat(25)).toBe("Overweight");
+  expect(fCat(25)).toBe("Overweight");
 });
 
 // **********************************************
 
 test('Testing "Overweight". Interior point.', () => {
-    expect(fCat(27.45)).toBe("Overweight");
+  expect(fCat(27.45)).toBe("Overweight");
 });
 
 test('Testing upper boundary of "Overweight". ON point.', () => {
-    expect(fCat(29.9)).toBe("Overweight");
+  expect(fCat(29.9)).toBe("Overweight");
 });
 
 test('Testing upper boundary of "Overweight". OFF point.', () => {
-    expect(fCat(30)).toBe("Obese");
+  expect(fCat(30)).toBe("Obese");
 });
 
 // **********************************************
 
 // Misc. Tests
-test('No input should be invalid', () => {
-    expect(fCat()).toBe("Invalid");
+test("No input should be invalid", () => {
+  expect(fCat()).toBe("Invalid");
 });
 
-test('Non-numeric input should be invalid', () => {
-    expect(fCat("Hello")).toBe("Invalid");
+test("Non-numeric input should be invalid", () => {
+  expect(fCat("Hello")).toBe("Invalid");
 });
