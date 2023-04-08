c8b8e66b63ec3e93e8782ed51e15f546995e07be
Maggie Pettus <gizademinuit@gmail.com>
2023-04-08 18:36:30 -0500

    Misc

--- calculateBMI.test.js
+++ calculateBMI.test.js
@@ -2,7 +2,7 @@ const cBMI = require("./calculateBMI.js");
 
 test("The result of the calculation should be a positive value", () => {
   expect(cBMI(0.1, 0.1, 0.1)).toBeGreaterThan(0);
-  });
+});
 
 test("The function should follow the BMI formula correctly", () => {
   expect(cBMI(5, 3, 125)).toBeCloseTo(22.68, 2);
